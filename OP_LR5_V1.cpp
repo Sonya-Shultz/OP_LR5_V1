4248dc472d89043f7a837721beb7a3ba82951f86
DezertRose <Dezertrouze@gmail.com>
2020-06-07 23:43:20 +0300

    Add some function

--- OP_LR5_V1.cpp
+++ OP_LR5_V1.cpp
@@ -1,4 +1,4 @@
-п»ї#include <iostream>
+#include <iostream>
 #include <vector>
 #include <fstream>
 #include <string>
@@ -12,21 +12,21 @@ const int max_dot = 50;
 const float R_e = 6371.008;
 
 struct FileData {
-	float lat; //С€РёСЂРѕС‚Р°
-	float longitude; //РґРѕР»РіРѕС‚Р°
-	string type;	//С‚РёРї
-	string subtype; //РїРѕРґС‚РёРї
-	string name; //РЅР°Р·РІР°РЅРёРµ
-	string address; // Р°РґСЂРµСЃР°
+	float lat = 0; //широта
+	float longitude = 0; //долгота
+	string type;	//тип
+	string subtype; //подтип
+	string name; //название
+	string address; // адреса
 };
 
 struct  Node
 {
 	vector<FileData> data;
-	Node* one, * two, * three, * four, *up;
+	Node* one, * two, * three, * four, * up;
 	bool is_see;
-	long int count_dot;
-	float slat, slon, hight, width; // slat, slon - Р»С–РІР° РІРµСЂС…РЅСЏ РєРѕРѕСЂРґРёРЅР°С‚Р° РїРѕ С€РёСЂРѕС‚С–, РїРѕ РґРѕРІРіРѕС‚С–; hight, wight - СЂРѕР·РјС–СЂ РєР»РІС‚РёРЅРєРё РїРѕ РґРѕРІРіРѕС‚С–, С€С‚СЂРѕС‚С–
+	long int count_dot=0;
+	float slat=0, slon=0, hight=0, width=0; // slat, slon - ліва верхня координата по широті, по довготі; hight, wight - розмір клвтинки по довготі, штроті
 };
 
 class Rtree {
@@ -46,13 +46,17 @@ private:
 	void fill_tree();
 	float distance(FileData dot1, FileData dot2);
 	FileData dot_for_look(FileData start, float alpha, float size_step);
-	vector<FileData> find_node(FileData dot, Node* nod, string type1, string subtype);
+	vector<FileData> find_node(FileData dot, Node* nod);
 	void show_data(FileData dot);
+	string deterStr(string str, int& i);
+	void ifFunc(Node** node, FileData dot);
 };
 
 void Rtree::show_data(FileData dot)
 {
-	cout <<dot.lat<<"; "<<dot.longitude<<"; "<< dot.type << "; " << dot.subtype << "; " << dot.name << "; " << dot.address <<";"<< endl;
+	cout << dot.lat << "; " << dot.longitude << "; " 
+		<< dot.type << "; " << dot.subtype << "; " 
+		<< dot.name << "; " << dot.address << ";" << endl;
 }
 
 Rtree::Rtree(string s) {
@@ -61,9 +65,18 @@ Rtree::Rtree(string s) {
 	maxlat = 57;
 	maxlon = 41;
 	file_name = s;
-	R_tree =NULL;
+	R_tree = NULL;
 	ReadFromFile();
 	fill_tree();
+}
+
+string Rtree::deterStr(string str, int& i) {
+	string tmplat;
+	while (str[i] != ';') {
+		tmplat += str[i];
+		i++;
+	}i++;
+	return tmplat;
 }
 
 void Rtree::ReadFromFile() {
@@ -76,32 +89,16 @@ void Rtree::ReadFromFile() {
 		while (!fin.eof()) {
 			getline(fin, str, '\n');
 			int i = 0;
-			while (str[i] != ';') {
-				tmplat += str[i];
-				i++;
-			}i++;
+			tmplat = deterStr(str, i);
 			help.lat = str_to_float(tmplat);
-			while (str[i] != ';') {
-				tmplong += str[i];
-				i++;
-			}i++;
+			
+			tmplong = deterStr(str, i);
 			help.longitude = str_to_float(tmplong);
-			while (str[i] != ';') {
-				help.type += str[i];
-				i++;
-			}i++;
-			while (str[i] != ';') {
-				help.subtype += str[i];
-				i++;
-			}i++;
-			while (str[i] != ';') {
-				help.name += str[i];
-				i++;
-			}i++;
-			while (str[i] != ';') {
-				help.address += str[i];
-				i++;
-			}i++;
+
+			help.type = deterStr(str, i);
+			help.subtype = deterStr(str, i);
+			help.name = deterStr(str, i);
+			help.address = deterStr(str, i);
 			dataV.push_back(help);
 			tmplat.clear(); tmplong.clear(); help.type.clear(); help.subtype.clear(); help.name.clear(); help.address.clear();
 		}
@@ -116,9 +113,29 @@ void Rtree::fill_tree()
 	}
 }
 
-void Rtree::push_new_dot(Node **nod, Node **parent, FileData dot)
+void Rtree::ifFunc( Node **nod, FileData dot) {
+	if ((*nod)->slon + ((*nod)->width / 2) > dot.longitude && (*nod)->slat - ((*nod)->hight / 2) > dot.lat)
+	{
+		push_new_dot(&((*nod)->one), &(*nod), dot);
+	}
+	if ((*nod)->slon + ((*nod)->width / 2) > dot.longitude && (*nod)->slat - ((*nod)->hight / 2) <= dot.lat)
+	{
+		push_new_dot(&((*nod)->three), &(*nod), dot);
+	}
+	if ((*nod)->slon + ((*nod)->width / 2) <= dot.longitude && (*nod)->slat - ((*nod)->hight / 2) > dot.lat)
+	{
+		push_new_dot(&((*nod)->two), &(*nod), dot);
+	}
+	if ((*nod)->slon + ((*nod)->width / 2) <= dot.longitude && (*nod)->slat - ((*nod)->hight / 2) <= dot.lat)
+	{
+		push_new_dot(&((*nod)->four), &(*nod), dot);
+	}
+
+}
+
+void Rtree::push_new_dot(Node** nod, Node** parent, FileData dot)
 {
-	if (*nod == NULL) 
+	if (*nod == NULL)
 	{
 		*nod = new Node;
 		(*nod)->one = NULL;
@@ -130,19 +147,19 @@ void Rtree::push_new_dot(Node **nod, Node **parent, FileData dot)
 			(*nod)->slon = minlon;
 			(*nod)->hight = maxlat - minlat;
 			(*nod)->width = maxlon - minlon;
-			(*nod)->data.push_back( dot);
-			(*nod)->count_dot =1;
+			(*nod)->data.push_back(dot);
+			(*nod)->count_dot = 1;
 			(*nod)->is_see = false;
 			(*nod)->up = NULL;
 			return;
 		}
 		else {
-			if ((*parent)->slon + ((*parent)->width / 2)  > dot.longitude) { (*nod)->slon = (*parent)->slon; }
-			else { (*nod)->slon = (*parent)->slon+ ((*parent)->width / 2) ; }
-			if ((*parent)->slat - ((*parent)->hight / 2)  > dot.lat) { (*nod)->slat = (*parent)->slat; }
-			else { (*nod)->slat = (*parent)->slat - ((*parent)->hight / 2) ; }
-			(*nod)->hight = ((*parent)->hight / 2) ;
-			(*nod)->width = ((*parent)->width / 2) ;
+			if ((*parent)->slon + ((*parent)->width / 2) > dot.longitude) { (*nod)->slon = (*parent)->slon; }
+			else { (*nod)->slon = (*parent)->slon + ((*parent)->width / 2); }
+			if ((*parent)->slat - ((*parent)->hight / 2) > dot.lat) { (*nod)->slat = (*parent)->slat; }
+			else { (*nod)->slat = (*parent)->slat - ((*parent)->hight / 2); }
+			(*nod)->hight = ((*parent)->hight / 2);
+			(*nod)->width = ((*parent)->width / 2);
 			(*nod)->data.push_back(dot);
 			(*nod)->count_dot = 1;
 			(*nod)->is_see = false;
@@ -151,48 +168,18 @@ void Rtree::push_new_dot(Node **nod, Node **parent, FileData dot)
 		}
 	}
 	else {
-		if ((*nod)->count_dot >= max_dot && (*nod)->one==NULL && (*nod)->two == NULL && (*nod)->three == NULL && (*nod)->four == NULL)
+		if ((*nod)->count_dot >= max_dot && (*nod)->one == NULL && (*nod)->two == NULL && (*nod)->three == NULL && (*nod)->four == NULL)
 		{
 			(*nod)->data.push_back(dot);
 			for (int it = 0; it < (*nod)->data.size(); it++) {
-				if ((*nod)->slon + ((*nod)->width / 2) > (*nod)->data[it].longitude && (*nod)->slat - ((*nod)->hight / 2)> (*nod)->data[it].lat)
-				{
-					push_new_dot(&((*nod)->one), &(*nod), (*nod)->data[it]);
-				}
-				if ((*nod)->slon + ((*nod)->width / 2) > (*nod)->data[it].longitude && (*nod)->slat - ((*nod)->hight / 2) <= (*nod)->data[it].lat)
-				{
-					push_new_dot(&((*nod)->three), &(*nod), (*nod)->data[it]);
-				}
-				if ((*nod)->slon + ((*nod)->width / 2)  <= (*nod)->data[it].longitude && (*nod)->slat - ((*nod)->hight / 2)  > (*nod)->data[it].lat)
-				{
-					push_new_dot(&((*nod)->two), &(*nod), (*nod)->data[it]);
-				}
-				if ((*nod)->slon + ((*nod)->width / 2)  <= (*nod)->data[it].longitude && (*nod)->slat - ((*nod)->hight / 2)  <= (*nod)->data[it].lat)
-				{
-					push_new_dot(&((*nod)->four), &(*nod), (*nod)->data[it]);
-				}
+				ifFunc(&*nod, (*nod)->data[it]);
 			}
 			(*nod)->data.clear();
 			return;
 		}
-		if ((*nod)->count_dot >= max_dot )
+		if ((*nod)->count_dot >= max_dot)
 		{
-			if ((*nod)->slon + ((*nod)->width / 2) > dot.longitude && (*nod)->slat - ((*nod)->hight / 2) > dot.lat)
-			{
-				push_new_dot(&((*nod)->one), &(*nod), dot);
-			}
-			if ((*nod)->slon + ((*nod)->width / 2) > dot.longitude && (*nod)->slat - ((*nod)->hight / 2) <= dot.lat)
-			{
-				push_new_dot(&((*nod)->three), &(*nod), dot);
-			}
-			if ((*nod)->slon + ((*nod)->width / 2) <= dot.longitude && (*nod)->slat - ((*nod)->hight / 2) > dot.lat)
-			{
-				push_new_dot(&((*nod)->two), &(*nod), dot);
-			}
-			if ((*nod)->slon + ((*nod)->width / 2) <= dot.longitude && (*nod)->slat - ((*nod)->hight / 2) <= dot.lat)
-			{
-				push_new_dot(&((*nod)->four), &(*nod), dot);
-			}
+			ifFunc(&*nod, dot);
 			return;
 		}
 		if ((*nod)->count_dot < max_dot) {
@@ -206,49 +193,41 @@ void Rtree::push_new_dot(Node **nod, Node **parent, FileData dot)
 
 float Rtree::distance(FileData dot1, FileData dot2)
 {
-	float dX = (cos(dot2.lat/57.4)) * (cos(dot2.longitude / 57.4)) - (cos(dot1.lat / 57.4)) * (cos(dot1.longitude / 57.4));
+	float dX = (cos(dot2.lat / 57.4)) * (cos(dot2.longitude / 57.4)) - (cos(dot1.lat / 57.4)) * (cos(dot1.longitude / 57.4));
 	float dY = (cos(dot2.lat / 57.4)) * (sin(dot2.longitude / 57.4)) - (cos(dot1.lat / 57.4)) * (sin(dot1.longitude / 57.4));
 	float dZ = sin(dot2.lat / 57.4) - sin(dot1.lat / 57.4);
 	float Cn = sqrt(dX * dX + dY * dY + dZ * dZ);
-	float alfa = acos(1 -( (R_e* R_e*Cn * Cn) / (2 * (R_e * R_e))));
+	float alfa = acos(1 - ((R_e * R_e * Cn * Cn) / (2 * (R_e * R_e))));
 	float l = R_e * alfa;
 	return l;
 }
 
-vector<FileData> Rtree::find_node(FileData dot, Node* nod, string type1, string subtype1)
+vector<FileData> Rtree::find_node(FileData dot, Node* nod)
 {
 	vector <FileData> help;
 	if ((nod)->one == NULL && (nod)->two == NULL && (nod)->three == NULL && (nod)->four == NULL)
 	{
-		if (!(nod)->is_see) { (nod)->is_see = true; help = nod->data; 
-			for (int i = 0; i < help.size(); i++) {
-				if (help[i].type == type1 && subtype1 == "")
-				{
-					show_data(help[i]);
-				}
-				if (help[i].type == type1 && subtype1 != "" && help[i].subtype == subtype1)
-				{
-					show_data(help[i]);
-				}
-			}
+		if (!(nod)->is_see) {
+			(nod)->is_see = true;
+			help = nod->data;
 		}
 		return help;
 	}
 	if ((nod)->slon + ((nod)->width / 2) > dot.longitude && (nod)->slat - ((nod)->hight / 2) > dot.lat)
 	{
-		find_node(dot, ((nod)->one), type1, subtype1);
+		help = find_node(dot, ((nod)->one));
 	}
 	if ((nod)->slon + ((nod)->width / 2) > dot.longitude && (nod)->slat - ((nod)->hight / 2) <= dot.lat)
 	{
-		find_node(dot, ((nod)->three), type1, subtype1);
+		help = find_node(dot, ((nod)->three));
 	}
 	if ((nod)->slon + ((nod)->width / 2) <= dot.longitude && (nod)->slat - ((nod)->hight / 2) > dot.lat)
 	{
-		find_node(dot, ((nod)->two), type1, subtype1);
+		help = find_node(dot, ((nod)->two));
 	}
 	if ((nod)->slon + ((nod)->width / 2) <= dot.longitude && (nod)->slat - ((nod)->hight / 2) <= dot.lat)
 	{
-		find_node(dot, ((nod)->four), type1, subtype1);
+		help = find_node(dot, ((nod)->four));
 	}
 	return help;
 }
@@ -263,7 +242,7 @@ FileData Rtree::dot_for_look(FileData start, float alpha, float size_step)
 
 void Rtree::find_around(float lat1, float lon1, float size, string type1, string subtype1)
 {
-	FileData start, now_dot; start.longitude = lon1; start.lat = lat1; 
+	FileData start, now_dot; start.longitude = lon1; start.lat = lat1;
 	float alpha;
 	vector <FileData> help;
 	for (alpha = 0; alpha < 360; alpha++)
@@ -272,7 +251,17 @@ void Rtree::find_around(float lat1, float lon1, float size, string type1, string
 		float step_size = 0.001;
 		while (distance(start, now_dot) < size)
 		{
-			help = find_node(now_dot, R_tree, type1, subtype1);
+			help = find_node(now_dot, R_tree);
+			for (int i = 0; i < help.size(); i++) {
+				if (help[i].type == type1 && subtype1 == "")
+				{
+					show_data(help[i]);
+				}
+				if (help[i].type == type1 && subtype1 != "" && help[i].subtype == subtype1)
+				{
+					show_data(help[i]);
+				}
+			}
 			step_size += 0.001;
 			now_dot = dot_for_look(start, alpha, step_size);
 		}
@@ -283,7 +272,7 @@ void Rtree::find_around(float lat1, float lon1, float size, string type1, string
 int main(int arg, char* argv[])
 {
 	setlocale(LC_ALL, "Russian");
-	string a, b,c,d,e,f="";
+	string a, b, c, d, e, f = "";
 	float lat, lon, size;
 	if (arg < 6) { cout << "Not all data \n"; }
 	else {
